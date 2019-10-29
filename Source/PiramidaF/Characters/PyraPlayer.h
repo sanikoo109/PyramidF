ffa4c45d3c62013a027728bfd6e39af2edd3f65c
sanikoo109 <sanikoo109@gmail.com>
2019-10-29 23:01:25 +0300

    InitialCommit

---
+++ Source/PiramidaF/Characters/PyraPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/PyraCharacter.h"
+#include "PyraPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIRAMIDAF_API APyraPlayer : public APyraCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PiramidaF/PyraGameMode.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PyraGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIRAMIDAF_API APyraGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+
+
+	
+};
