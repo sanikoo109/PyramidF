c6e0d3cc7c03450597809eaf34196230dd9e3991
sanikoo109 <sanikoo109@gmail.com>
2019-10-29 23:15:22 +0300

    CommitOnly 1 and 3

--- PiramidaF/Characters/PyraCharacter.h
+++ PiramidaF/Characters/PyraCharacter.h
@@ -26,4 +26,10 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Pyra Character")
+		bool UserChanges1;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Pyra Character")
+		bool UserChanges3;
+
 };
