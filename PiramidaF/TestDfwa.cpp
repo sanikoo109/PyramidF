6be32b99bdaf6e9f2d42070a3fdff89b9e06bc00
sanikoo109 <sanikoo109@gmail.com>
2019-10-31 20:00:19 +0300

    cwa
    ds

---
+++ PiramidaF/TestDfwa.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestDfwa.h"
+
+// Sets default values
+ATestDfwa::ATestDfwa()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestDfwa::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATestDfwa::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
